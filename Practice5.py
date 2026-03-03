3b6ea259799d6deed29cfa6b0afbf309a8d92a4b
KSaikumar04 <saikeshireddy@gmail.com>
2026-03-03 17:30:46 +0530

    Add files via upload

---
+++ Practice5.py
@@ -0,0 +1,8 @@
+"""#write a program to check whether a string is palindrome
+name = input("what is your name? ")
+colour = input("what is your faviourite colour? ")
+print(name + " likes " + colour)
+"""
+weight_pounds = input("enter your weight in pounds:")
+weight_kg = float(weight_pounds)*0.45
+print("your weight in kg = " + str(weight_kg))
